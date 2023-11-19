7d354f92775b34ddaaa42247bb482cad2931cdc9
Benature <wbenature@163.com>
2023-11-19 12:32:17 +0800

    requirements

--- api.py
+++ api.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
+from config import *
 
 app = Flask(__name__)
+
+from sender import *
+
+
+@app.route("/cubox", methods=['POST'])
+def cubox_func():
+    data = request.get_json()
+    cubox_new(data['url'])
+    # Process the JSON data here
+    # ...
+    return jsonify({"message": "Success"})
 
 
 @app.route("/")
@@ -9,4 +22,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8642)
+    app.run(debug=True, host="0.0.0.0", port=8642)
